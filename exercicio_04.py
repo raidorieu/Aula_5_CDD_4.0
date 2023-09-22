b004ca8f39f9f90f30de0ed063d8c97fd6267125
Bernardo Guerra <bernardoguerra93@gmail.com>
2023-09-22 08:19:13 -0300

    fiz o quarto exercicio

--- exercicio_04.py
+++ exercicio_04.py
@@ -4,7 +4,7 @@ senha=int(input("Digite sua senha: "))
 tentativas=1
 while senha!=senhasecreta:
     if tentativas<3:
-        senha = int(input("Senha incorreta, digite novamente: "))
+        senha = int(input(f"Senha incorreta, restam {3-tentativas} tentativas, digite novamente: "))
         tentativas+=1
     else:
         print("Limite de tentativas alcançado")
